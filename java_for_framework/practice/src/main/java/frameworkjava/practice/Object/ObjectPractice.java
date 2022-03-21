5097719557d7480306d05cdcbb32f8729dcc6bda
jki503 <jki503@naver.com>
2022-03-21 23:34:41 +0900

    [MODIFY] change directory

---
+++ java_for_framework/practice/src/main/java/frameworkjava/practice/Object/ObjectPractice.java
@@ -0,0 +1,18 @@
+package frameworkjava.practice.Object;
+
+import frameworkjava.practice.Unit;
+
+public class ObjectPractice {
+
+    public static void main(String[] args) {
+
+        Unit unit = new Unit(1L,20L,"monster", 500L);
+        Unit unit2 = new Unit(1L,20L,"monster", 500L);
+
+        System.out.println(unit); // toString이 정의 되지 않으면 instance 반환
+
+        System.out.println(unit.equals(unit2)); // 객체 비교
+
+
+    }
+}
---
+++ java_for_framework/practice/src/main/java/frameworkjava/practice/String/StringBufferPractice.java
@@ -0,0 +1,24 @@
+package frameworkjava.practice.String;
+
+public class StringBufferPractice {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("abc");
+        sb.setLength(2);
+        System.out.println(sb.toString()); // ab 출력
+
+        sb.insert(1,"c");
+        System.out.println(sb.toString()); // acb 출력
+        System.out.println(sb.length()); // 3 출력
+
+        sb.deleteCharAt(1);
+
+        System.out.println(sb.toString()); // ab 출력
+        System.out.println(sb.substring(0,2)); // ab
+        System.out.println(sb.substring(0)); // ab
+        System.out.println(sb.compareTo(new StringBuffer("ab"))); // 0
+
+    }
+}
