f83c6a86de6f7b3f811e83dc9cabfb367a559038
jki503 <jki503@naver.com>
2022-05-17 00:14:08 +0900

    [ADD] mission1 스스로 해보기

--- springboot-security/ssmc/src/main/java/com/prgrms/devcourse/configuration/WebSecurityConfiguration.java
+++ springboot-security/ssmc/src/main/java/com/prgrms/devcourse/configuration/WebSecurityConfiguration.java
@@ -1,14 +1,23 @@
 package com.prgrms.devcourse.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -16,15 +25,47 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-          .authorizeRequests()
-            .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                .anyRequest().permitAll()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        String userPassword = "user123";
+        String adminPassword = "admin123";
+
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode(userPassword))
+                .roles("USER")
+
                 .and()
-            .formLogin()
+                .withUser("admin")
+                .password(passwordEncoder.encode(adminPassword))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+
+                .and()
+                .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
-                .and();
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll()
+
+                .and()
+                .rememberMe()
+                .key("unique")
+                .rememberMeParameter("remember-me")
+                .userDetailsService(userDetailsService)
+                .tokenValiditySeconds(60*5);
     }
+
+
 }
