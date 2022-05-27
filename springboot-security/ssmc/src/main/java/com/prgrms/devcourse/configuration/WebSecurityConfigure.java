6cdeca18f14cbc6845cf59c494c29d461ae7b610
jki503 <jki503@naver.com>
2022-05-27 15:50:16 +0900

    jpa 사용 이전 spring security day 6

--- springboot-security/ssmc/src/main/java/com/prgrms/devcourse/configuration/WebSecurityConfigure.java
+++ springboot-security/ssmc/src/main/java/com/prgrms/devcourse/configuration/WebSecurityConfigure.java
@@ -2,8 +2,14 @@ package com.prgrms.devcourse.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
@@ -17,12 +23,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,36 +45,78 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(WebSecurityConfigure.class);
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/assets/**");
+    private final DataSource dataSource;
+
+    public WebSecurityConfigure(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                    .withUser("user")
-                    .password("{noop}user123")
-                    .roles("USER")
-                .and()
-                    .withUser("admin01")
-                    .password("{noop}admin123")
-                    .roles("ADMIN")
-                .and()
-                    .withUser("admin02")
-                    .password("{noop}admin123")
-                    .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT " +
+                                "login_id, passwd, true " +
+                                "FROM " +
+                                "USERS " +
+                                "WHERE " +
+                                "login_id = ?"
+                )
+                .groupAuthoritiesByUsername(
+                        "SELECT " +
+                                "u.login_id, g.name, p.name " +
+                                "FROM " +
+                                "users u JOIN groups g ON u.group_id = g.id " +
+                                "LEFT JOIN group_permission gp ON g.id = gp.group_id " +
+                                "JOIN permissions p ON p.id = gp.permission_id " +
+                                "WHERE " +
+                                "u.login_id = ?"
+                )
+                .getUserDetailsService().setEnableAuthorities(false);
     }
 
-    public SecurityExpressionHandler<FilterInvocation> securityExpressionHandler(){
+
+    @Bean
+    @Qualifier("myAsyncTaskExecutor")
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setThreadNamePrefix("my-executor-");
+        return executor;
+    }
+
+    @Bean
+    public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(
+            @Qualifier("myAsyncTaskExecutor") AsyncTaskExecutor delegate
+    ) {
+        return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
+    }
+
+//    public WebSecurityConfigure() {
+//        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+//    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**", "/h2-console/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public SecurityExpressionHandler<FilterInvocation> securityExpressionHandler() {
         return new CustomWebSecurityExpressionHandler(
-            new AuthenticationTrustResolverImpl(),
+                new AuthenticationTrustResolverImpl(),
                 "ROLE_"
         );
     }
 
     @Bean
-    public AccessDecisionManager accessDecisionManager(){
+    public AccessDecisionManager accessDecisionManager() {
         List<AccessDecisionVoter<?>> voters = new ArrayList<>();
         voters.add(new WebExpressionVoter());
         voters.add(new OddAdminVoter(new AntPathRequestMatcher("/admin")));
@@ -77,13 +131,13 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .accessDecisionManager(accessDecisionManager())
                 //.expressionHandler(securityExpressionHandler())
-                    .and()
+                .and()
                 .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
-                    .and()
+                .and()
                 .httpBasic()
-                    .and()
+                .and()
                 /**
                  *  로그아웃 설정
                  * */
@@ -93,22 +147,22 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .permitAll()
-                    .and()
+                .and()
                 /**
                  *  remember me 설정
                  * */
                 .rememberMe()
                 .key("my-remember-me")
                 .rememberMeParameter("remember-me")
-                .tokenValiditySeconds(60*5)
+                .tokenValiditySeconds(60 * 5)
                 .alwaysRemember(false)
-                    .and()
+                .and()
                 /**
                  *  HTTP 요청을 HTTPS 요청으로 리다이렉트
                  * */
                 .requiresChannel()
                 .anyRequest().requiresSecure()
-                    .and()
+                .and()
                 /**
                  *  Session Management
                  * */
@@ -119,20 +173,20 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .maximumSessions(1)
                 .maxSessionsPreventsLogin(false)
                 .and()
-                    .and()
+                .and()
                 /**
                  *  Anonymous
                  * */
                 .anonymous()
                 .principal("thisIsAnonymousUser")
                 .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN")
-                    .and()
+                .and()
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler());
     }
 
     @Bean
-    public AccessDeniedHandler accessDeniedHandler(){
+    public AccessDeniedHandler accessDeniedHandler() {
         return (request, response, e) -> {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             Object principal = authentication != null ? authentication.getPrincipal() : null;
